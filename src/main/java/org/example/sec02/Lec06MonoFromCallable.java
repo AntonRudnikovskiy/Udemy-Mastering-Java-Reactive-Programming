6e7d4930f24ff838927a69450de91661567bfcfe
Anton Rudnikovskiy <mkasadasa@yandex.ru>
2024-08-23 11:49:45 +0300

    Lec06MonoFromCallable

---
+++ src/main/java/org/example/sec02/Lec06MonoFromCallable.java
@@ -0,0 +1,25 @@
+package org.example.sec02;
+
+import org.example.common.Util;
+import org.example.sec01.publisher.SubscriptionImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class Lec06MonoFromCallable {
+    private static final Logger log = LoggerFactory.getLogger(SubscriptionImpl.class);
+
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(1, 2, 3);
+        Mono.fromCallable(() -> sum(integers)).subscribe(Util.subscriber());
+    }
+
+    private static int sum(List<Integer> list) throws Exception{
+        log.info("finding the sum of {}", list);
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+}
