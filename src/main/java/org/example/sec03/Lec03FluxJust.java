9901189cfab61d6fc4930ed59f0dcc0c375cefd4
Anton Rudnikovskiy <mkasadasa@yandex.ru>
2024-08-24 16:38:21 +0300

    Lec03FluxJust

---
+++ src/main/java/org/example/sec03/Lec03FluxJust.java
@@ -0,0 +1,12 @@
+package org.example.sec03;
+
+import org.example.common.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec03FluxJust {
+    public static void main(String[] args) {
+        Flux<Integer> flux = Flux.just(1, 3, 6, 7);
+
+        flux.subscribe(Util.subscriber());
+    }
+}
