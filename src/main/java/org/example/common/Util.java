8538f9e22b83a48e0cf5b0485831e1cdac5b3bfd
Anton Rudnikovskiy <mkasadasa@yandex.ru>
2024-08-25 17:35:48 +0300

    Lec01FluxCreate

--- src/main/java/org/example/common/Util.java
+++ src/main/java/org/example/common/Util.java
@@ -1,13 +1,20 @@
 package org.example.common;
 
+import com.github.javafaker.Faker;
 import org.reactivestreams.Subscriber;
 
 public class Util {
+    private static final Faker faker = Faker.instance();
+
     public static <T> Subscriber<T> subscriber() {
         return new DefaultSubscriber<>("");
     }
 
     public static <T> Subscriber<T> subscriber(String name) {
         return new DefaultSubscriber<>(name);
+    }
+
+    public static Faker faker(){
+        return faker;
     }
 }
